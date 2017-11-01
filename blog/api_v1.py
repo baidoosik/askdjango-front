c5b8384f47632c19051b098bf707d974b3c2f33b
baidoosik <qoentlr37@naver.com>
2017-11-01 22:41:11 +0900

    djangorestframework의 클래스 api view로 post list 구현

--- blog/api_v1.py
+++ blog/api_v1.py
@@ -3,16 +3,11 @@ from rest_framework.renderers import JSONRenderer
 from .serializers import PostSerializer
 from .models import Post
 from django.conf.urls import url
-from django.views.generic import ListView
+from rest_framework.pagination import PageNumberPagination
+from rest_framework import generics
 
 
-# def post_list(request):
-#     qs = Post.objects.all()
-#     serializer = PostSerializer(qs, many=True)
-#     json_utf8_string = JSONRenderer().render(serializer.data)
-#     return HttpResponse(json_utf8_string, content_type='application/json',
-#                         charset='utf-8')
-
+# 함수형
 def post_list(request):
     qs = Post.objects.all()
     serializer = PostSerializer(qs, many=True)
@@ -20,6 +15,17 @@ def post_list(request):
     return HttpResponse(json_utf8_strging,
                         content_type='application/json; charset=utf8')
 
+
+class PostPagination(PageNumberPagination):
+    page_size = 10
+
+
+class PostListAPIView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    pagination_class = PostPagination
+
 urlpatterns = [
-    url(r'^post/list/$', post_list, name='api_v1_post_list')
+    url(r'^post/list/$', post_list, name='api_v1_post_list'),
+    url(r'^post/list_json/$', PostListAPIView.as_view(), name='api_v1_post_list_json')
 ]
